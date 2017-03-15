e0338192ce601987e3386da80c047f33497cc72d
Christoph Fabianek <christoph.fabianek@gmail.com>
2017-03-15 15:37:12 +0100

    template merge

--- uiPiaConfig.R
+++ uiPiaConfig.R
@@ -1,6 +1,8 @@
 uiPiaConfig <- function(){
         tabPanel(HTML(paste0('Verbindung zum Datentresor</a></li>',
-                             '<li><a href="', desktopUrl, '">zur Desktop Version')),
+                             '<li><a id="desktoplink" href="', 
+                             desktopUrl, 
+                             '">zur Desktop Version')),
                  h3('Datentresor'),
                  textInput('pia_urlMobile', 'Adresse:'),
                  textInput('app_keyMobile', 'Identifier:'),
