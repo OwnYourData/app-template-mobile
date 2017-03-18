1528eb7780ebe6d4d4e1197979bbc9d70d8f0704
Christoph Fabianek <christoph.fabianek@gmail.com>
2017-03-18 23:49:39 +0100

    merge

--- appConstants.R
+++
@@ -1,8 +0,0 @@
-# global constants available to the mobile app
-# last update:2016-10-17
-
-# constants required for every app
-appName <- 'mobile-template'
-appTitle <- 'Mobiles Template'
-app_id <- 'eu.ownyourdata.template'
-desktopUrl <- 'https://template.datentresor.org'
--- appNewData.R
+++
@@ -1,8 +0,0 @@
-# mobile UI to collect data
-# last update: 2017-03-01
-
-appNewData <- function(){
-        tabPanel('Datenerfassung',
-                 helpText('Erfasse hier einen neuen Eintrag im Template.')
-        )
-}
